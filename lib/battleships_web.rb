5163bebe09bb01ba701210b8442d915524f333ad
Gerard Morera <grdmorera@gmail.com>
2015-06-22 15:21:28 +0100

    working with first feature test

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -2,9 +2,12 @@ require 'sinatra/base'
 
 class BattleshipsWeb < Sinatra::Base
   get '/' do
-    'Hello BattleshipsWeb!'
+    erb :index
   end
+  
+  set :views, proc { File.join(root, '..', 'views') }
 
   # start the server if ruby file executed directly
   run! if app_file == $0
+
 end
