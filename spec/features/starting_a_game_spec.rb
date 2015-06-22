a3d928ed0d26372cb90cc633d8e82f08735235cf
Natalie Akam <natalie.akam@gmail.com>
2015-06-22 15:12:30 +0100

    Added start page

--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature 'Starting a new game' do
-  scenario 'Starting a new game' do
+  scenario 'I am asked to enter my name' do
     visit '/'
     click_link 'New Game'
     expect(page).to have_content "What's your name?"
