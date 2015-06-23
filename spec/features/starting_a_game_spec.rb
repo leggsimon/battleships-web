12ff0b77ed8fc85fffcfcc68e39776ec90bf7fbb
Elin <elinnet@gmail.com>
2015-06-23 13:13:37 +0100

    update of files following git messaget that files not staged for commit

--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -6,4 +6,15 @@ feature 'Starting a new game' do
     click_link 'New Game'
     expect(page).to have_content "What's your name?"
   end
+
+  scenario 'Enter name details into form' do
+    visit '/new_game' do
+      params={:name =>'Bob'}
+    expect(form).to have_content "Bob"
+
+
+
+
+
+
 end
