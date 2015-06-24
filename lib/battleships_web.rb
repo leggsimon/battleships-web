bf49b8911fe0d74b9efbbe2d6f621bec899d83bb
Simon Legg <leggsimon@gmail.com>
2015-06-24 12:11:30 +0100

    Can place a ship on the board

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -12,18 +12,30 @@ class BattleshipsWeb < Sinatra::Base
     erb :name
   end
 
-  get '/welcome' do
-    @player_name = params[:name]
-    erb :welcome
-    $game = Game.new Player, Board
+  post '/welcome' do
+    if params[:name] != ''
+      @player_name = params[:name]
+      $game = Game.new Player, Board
+      erb :welcome
+    else
+      redirect '/name'
+    end
   end
 
-  get '/board' do
-    erb :board
+  get '/current_game' do
+    erb :game
   end
 
+  post '/placed_ship' do
+    @coordinates = params[:Coordinates]
+    @shiptype    = params[:shipTypes]
+    @orientation = params[:orientation]
+    # ships_hash = {destoyer => Ship.destoyer}
 
+    $game.player_1.place_ship Ship.battleship, :B4, :vertically
 
+    erb :game
+  end
 
 
   # start the server if ruby file executed directly
--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -9,17 +9,37 @@ feature 'Starting a new game' do
 
   scenario 'Enter name details into form. Reports hello' do
     visit '/name'
-    fill_in('name', with: 'Bob')
+    fill_in('name', with: 'John')
     click_button('Submit')
-    expect(page).to have_content('Bob')
+    expect(page).to have_content('John')
   end
 
   scenario 'If no name is entered you are given link back to enter your name' do
     visit '/name'
     click_button('Submit')
-    click_link 'Please click here to enter your name'
     expect(current_url).to have_content('/name')
   end
 
+  scenario 'Displays board when you start a game' do
+    go_to_welcome_page
+    click_button("Start Game")
+    expect(page).to have_content('ABCDEFGHIJ')
+  end
+
+  scenario 'Can place a ship' do
+    go_to_welcome_page
+    click_button("Start Game")
+    fill_in('Coordinates', with: 'C3')
+    select "Destroyer", from: "shipTypes"
+    select "Vertically", from: "orientation"
+    click_button"Place Ship"
+    expect(page).to have_content("ABCDEFGHIJ ------------ 1| |1 2| |2 3| D |3 4| D |4 5| |5 6| |6 7| |7 8| |8 9| |9 10| |10 ------------ ABCDEFGHIJ")
+  end
+
+  def go_to_welcome_page
+    visit '/name'
+    fill_in('name', with: 'Bob')
+    click_button('Submit')
+  end
 
 end
